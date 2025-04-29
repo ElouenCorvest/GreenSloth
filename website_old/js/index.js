1f7c96f6450cf4b7f556b507ec8fb87591e17a29
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2025-04-29 16:08:01 +0200

    dev: chg: finished swiper

--- website_old/js/index.js
+++ website_old/js/index.js
@@ -1,6 +1,9 @@
-function mapClick(evt) {
-    console.log("hello")
-}
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import Navigation from"https://cdn.jsdelivr.net/npm/swiper@11/modules/navigation.min.mjs"
+import Pagination from"https://cdn.jsdelivr.net/npm/swiper@11/modules/pagination.min.mjs"
+
+// Register the Navigation module
+Swiper.use([Navigation, Pagination]);
 
 // import Swiper from 'swiper'
 
@@ -10,7 +13,21 @@ var swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     slidesPerView: 1,
     grabCursor: true,
+    autoHeight: true,
     loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-next-button',
+        prevEl: '.swiper-prev-button'
+    },
+
+    //Dots
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: "bullets"
+    },
     // freeMode: true,
     // freeModeSticky: true
   });
