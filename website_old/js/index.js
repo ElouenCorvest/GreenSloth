7f43c0fd4aa239f0fca2735a18ece02fe55a9c09
Elouen_Corvest <elouen.corvest@rwth-aachen.de>
2025-05-19 15:46:12 +0200

    dev: chg: made the links in the photosynthesis scheme

--- website_old/js/index.js
+++ website_old/js/index.js
@@ -36,3 +36,27 @@ var swiper = new Swiper('.swiper-container', {
       },
 
   });
+
+// Photosynthesis Scheme
+
+// Photosynthesis Scheme - Set the links
+var schemePsii = document.getElementById("scheme-psii")
+schemePsii.setAttribute("href", "models_select.html?appar=PSII")
+
+var schemeCytb6f = document.getElementById("scheme-cytb6f")
+schemeCytb6f.setAttribute("href", "models_select.html?appar=Cytb6f")
+
+var schemePsi = document.getElementById("scheme-psi")
+schemePsi.setAttribute("href", "models_select.html?appar=PSI")
+
+var schemeAtpsynth = document.getElementById("scheme-atpsynth")
+schemeAtpsynth.setAttribute("href", "models_select.html?appar=ATPSynth")
+
+var schemeCBB = document.getElementById("scheme-cbb")
+schemeCBB.setAttribute("href", "models_select.html?appar=CBB")
+
+var schemeFNR = document.getElementById("scheme-fnr")
+schemeFNR.setAttribute("href", "models_select.html?appar=FNR")
+
+var schemeOEC = document.getElementById("scheme-oec")
+schemeOEC.setAttribute("href", "models_select.html?appar=OEC")
