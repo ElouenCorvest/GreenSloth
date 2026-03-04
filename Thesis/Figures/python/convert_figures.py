3876aaeb4da9b27b6e4880bc7a3ae96eccc6642b
ElouenCorvest <elouen2000@gmail.com>
2026-03-04 15:07:52 +0100

    dev: chg: added horizontal demonstrations

--- Thesis/Figures/python/convert_figures.py
+++ Thesis/Figures/python/convert_figures.py
@@ -112,6 +112,8 @@ for sim, models in demon_dict.items():
         else:
             grid_drawings.append([models[model_name]["drawing"]])
             grid_names.append([model_name.capitalize()])
+            
+    
     title_height_add = 40
     spacing_between = 40
     page_height = 0
@@ -148,7 +150,68 @@ for sim, models in demon_dict.items():
             c.drawString(text_x, y_offset + drawing.height + 5, title)
             x_offset += drawing.width + spacing_between
             
+    c.save()
+
+for sim, models in demon_dict.items():
     
+    output_path = Path(__file__).parents[1] / "Demonstrations_hori" / f"{sim}.pdf"
+    
+    num_cols = 3
+    
+    lst_names = [model for model in models.keys()]
+    lst_names = sorted(lst_names)
+    
+    grid_drawings = []
+    grid_names = []
+    
+    for model_name in lst_names:
+        if grid_drawings == []:
+            grid_drawings.append([models[model_name]["drawing"]])
+            grid_names.append([model_name.capitalize()])
+        elif len(grid_drawings[-1]) < num_cols:
+            grid_drawings[-1].append(models[model_name]["drawing"])
+            grid_names[-1].append(model_name.capitalize())
+        else:
+            grid_drawings.append([models[model_name]["drawing"]])
+            grid_names.append([model_name.capitalize()])
+            
+    
+    title_height_add = 40
+    spacing_between = 40
+    page_height = 0
+    page_width = 0
+    for row in grid_drawings:
+        row_height = max([drawing.height for drawing in row])
+        row_height += title_height_add
+        page_height += row_height
+        
+        row_width = sum([drawing.width for drawing in row])
+        page_width = max(row_width, page_width)
+
+    page_width += spacing_between
+    c = canvas.Canvas(str(output_path), pagesize=(page_width, page_height))
+    fontsize = 24
+    c.setFont("Helvetica-Bold", fontsize)
+    
+    for i, row in enumerate(grid_drawings):
+        row_height = max([drawing.height for drawing in row])
+        row_width = sum([drawing.width for drawing in row])
+        x_offset = (page_width - spacing_between - row_width) / 2
+        y_offset = page_height - (row_height + title_height_add) * (i + 1)
+    
+        
+        for j, drawing in enumerate(row):
+            renderPDF.draw(drawing, c, x_offset, y_offset)
+            
+            title = grid_names[i][j]
+            text_width = c.stringWidth(title, "Helvetica-Bold", fontsize)
+            if "mca" in sim.lower():
+                text_x = x_offset + (drawing.width - text_width) / 2
+            else:
+                text_x = x_offset + (drawing.width * 0.1)
+            c.drawString(text_x, y_offset + drawing.height + 5, title)
+            x_offset += drawing.width + spacing_between
+            
     c.save()
     
 
